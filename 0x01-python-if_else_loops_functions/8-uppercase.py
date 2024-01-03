044747bdb90a27aecf64946162873984f6e2d148
Matthew Thiga <matthewthiga72@gmail.com>
2024-01-03 12:28:35 -0800

    First push for 9

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,6 +2,8 @@
 def uppercase(str):
     for i in str:
         if (ord(i) > 96 and ord(i) < 123):
-            print("{}".format(i))
+            print("{}".format(i), end="")
         else:
-            print("{}".format(chr(ord(i) - 32)))
+            print("{}".format(chr(ord(i) - 32)), end="")
+
+uppercase("hello")
