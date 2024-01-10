ecfc521d3d46729742a70b95ee609941f2369806
Matthew Mwangi <matthewthiga72@gmail.com>
2024-01-10 11:54:11 +0100

    First push for 0, 1, 2, 3, 4, 5, 6, 7, 8, 9

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = [row.copy() for row in matrix]
+    for i in range(len(new_matrix)):
+        for j in range(len(new_matrix[i])):
+            new_matrix[i][j] *= new_matrix[i][j]
+    return new_matrix
