ace7d0190ef9c2b12ff38add8842118fccbacf1d
Matthew Mwangi <matthewthiga72@gmail.com>
2024-01-10 20:24:03 +0100

    Adapted code to change for all occurences

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = my_list.copy()
-    new_list[search] = replace
+    for i in range(len(new_list)):
+        if new_list[i] == search:
+            new_list[i] = replace
     return new_list
