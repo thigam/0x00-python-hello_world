8ebb51db7eeb6302c9b1e7cb2d2033b6de4de261
Matthew Mwangi <matthewthiga72@gmail.com>
2024-01-10 20:19:23 +0100

    First push for a complete 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,45 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None or not roman_string="":
+    if roman_string is None or not roman_string:
         return 0
     sum = 0
-    for i in roman_string:
-        if i == 
+    for i in range(len(roman_string) - 1):
+        if roman_string[i] == 'M':
+            sum += 1000
+        elif roman_string[i] == 'D':
+            sum += 500
+        elif roman_string[i] == 'C':
+            if roman_string[i + 1] == 'D' or roman_string[i + 1] == 'M':
+                sum -= 100
+            else:
+                sum += 100
+        elif roman_string[i] == 'L':
+            sum += 50
+        elif roman_string[i] == 'X':
+            if roman_string[i + 1] == 'L' or roman_string[i + 1] == 'C':
+                sum -= 10
+            else:
+                sum += 10
+        elif roman_string[i] == 'V':
+            sum += 5
+        elif roman_string[i] == 'I':
+            if roman_string[i + 1] == 'X' or roman_string[i + 1] == 'V':
+                sum -= 1
+            else:
+                sum += 1
+    if roman_string[-1] == 'M':
+        sum += 1000
+    if roman_string[-1] == 'D':
+        sum += 500
+    if roman_string[-1] == 'C':
+        sum += 100
+    if roman_string[-1] == 'L':
+        sum += 50
+    if roman_string[-1] == 'X':
+        sum += 10
+    if roman_string[-1] == 'V':
+        sum += 5
+    if roman_string[-1] == 'I':
+        sum += 1
+    return sum
+
