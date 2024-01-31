0b0616c167bff1bc9f9123f793f69cf3fc407cce
Matthew Mwangi <matthewthiga72@gmail.com>
2024-01-31 10:49:40 +0100

    Corrected pycodestyle error

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -7,6 +7,8 @@ A module containing a locked class
 class LockedClassParent:
     """Parent class with attribute restriciton"""
     __slots__ = ('first_name')
+
+
 class LockedClass(LockedClassParent):
     """
     Empty class
